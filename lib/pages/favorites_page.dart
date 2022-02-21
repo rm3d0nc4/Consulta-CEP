160df8167d24a9cd5af2970ba5d769d50c0bb415
rm3d0nc4 <romeromendonca22@gmail.com>
2022-02-21 14:01:57 -0300

    Criando FavoriteItem

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -7,25 +7,57 @@ class FavoritesPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF1C85A8),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.arrow_back_rounded)),
-          centerTitle: true,
-          title: const Text(
-            'Favoritos',
-            style: TextStyle(
-                fontSize: 23,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.bold),
-          ),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C85A8),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_rounded)),
+        centerTitle: true,
+        title: const Text(
+          'Favoritos',
+          style: TextStyle(
+              fontSize: 23, fontFamily: 'Poppins', fontWeight: FontWeight.bold),
         ),
-        body: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ListView.builder(
-                itemBuilder: (BuildContext context, index) => Container())));
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            FavoriteItem(),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+class FavoriteItem extends StatelessWidget {
+  const FavoriteItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Row(
+      children: [
+        Container(
+          height: height * .066,
+          width: width * .64,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Color(0xFFFFFFFF),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [Icon(Icons.info_rounded), Text('64865-000')],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+// 390x844
