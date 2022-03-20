34a9af48a3b90400145cb5bf37bfa4f31c086cca
rm3d0nc4 <romeromendonca22@gmail.com>
2022-03-19 22:56:46 -0300

    Criando exception para a classe cepApi

--- lib/api/cep_api.dart
+++ lib/api/cep_api.dart
@@ -1,15 +1,29 @@
 import 'dart:convert';
 
-import 'package:consulta_cep/models/cep_model.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:consulta_cep/models/cep_model.dart';
+
 class CepApi {
-  Future<CepModel> getDataWithCep(String cep) async {
-    String url = 'https://viacep.com.br/ws/${cep}/json/';
+  Future getDataWithCep(String cep) async {
+    String url = 'https://viacep.com.br/ws/$cep/json/';
     Uri uri = Uri.parse(url);
     http.Response response = await http.get(uri);
     String body = response.body;
     CepModel cepObject = CepModel.fromJson(body);
-    return cepObject;
+    print(cepObject.cep.isEmpty);
+    if (cepObject.cep.isEmpty) {
+      throw CepException(
+          'Não foi possível encontrar dados para o CEP informado');
+    } else {
+      return cepObject;
+    }
   }
+}
+
+class CepException implements Exception {
+  String message;
+  CepException(
+    this.message,
+  );
 }
